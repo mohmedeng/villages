2b58b4c484c88a5bf404783e89c6182ee3576082
mohmedeng <mohmed.abdelkhaleq@gmail.com>
2023-01-19 12:16:40 +0100

    local

--- ittakesavillage/settings.py
+++ ittakesavillage/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
         'NAME': 'village', 
         'USER': 'admin', 
         'PASSWORD': '123',
-        'HOST': '127.0.0.1', 
+        'HOST': '0.0.0.0', 
         'PORT': '5432',
     }
 }
