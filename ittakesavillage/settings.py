450cbc4cbb515e638607e9edcd163da727be423a
mohmedeng <mohmed.abdelkhaleq@gmail.com>
2023-01-19 22:29:02 +0100

    jjj

--- ittakesavillage/settings.py
+++ ittakesavillage/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'ittakesavillage.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'village', 
         'USER': 'admin', 
         'PASSWORD': '123',
